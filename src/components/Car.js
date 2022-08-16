d0212341be0f6c2a9ec99bff01e090147eecc685
RosalynS78 <rrcervania78@gmail.com>
2022-08-15 21:41:52 -0500

    push

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,14 +1,31 @@
 import React from 'react'
 import cars from '../cars.json'
-// Import {useParams} from "react-router-dom" here //
+import { useParams } from 'react-router-dom';
 
 // import MUI components here //
-// Container, Paper, Chip //
+import { Container, Paper, Chip } from "@mui/material";
 
 const Car = (props) => {
-    return (
-        <h1>A specific car</h1>
-    )
+  const id = useParams().id
+ 
+  const car = cars.find((car) => car.id === Number(id));
+
+  return (
+    // Container, Paper, Chip //
+    <Container maxWidth="sm" className="car-container">
+      <Paper className="car-paper">
+        <h1> {car.Name} </h1>
+        {/* // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys */}
+        {Object.keys(car).map((key, idx) => {
+          return <Chip key={idx} label={`${key}: ${car[key]}`}></Chip>;
+        })}
+      </Paper>
+    </Container>
+
+
+
+  )
+ 
 }
 
 export default Car
